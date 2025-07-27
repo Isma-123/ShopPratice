018a71987203b463275f068274537a90dae249ec
Abdias <felizseguraabdias@gmail.com>
2025-07-27 12:01:33 -0400

    Inyeccion de dependecy

---
+++ InjeccionIOS/Dependecy/AddDependency.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using StoryDataBase.cs.Context;
+using StoryDataBase.cs.Intefaces;
+using StoryDataBase.cs.Repository.cs;
+
+namespace InjeccionIOS.Dependecy
+{
+    public static class AddDependency
+    { 
+
+      public static IServiceCollection adddependecy(this IServiceCollection services)
+        {
+
+
+            services.AddScoped<ICategoriesDates, CategoryRepository>();
+            services.AddScoped<IOrderDates, OrderRepository>(); 
+            services.AddScoped<IOrderDetailsDates, OrderDetailsRepository>();
+            services.AddScoped<IProductDates, ProductRepository>();
+            services.AddScoped<IUserDates, UserRepository>();
+    
+
+            return services;
+        }
+
+    }
+}
